418a06ee6e5872ebf1dc3ff4beb1aee1f06a7c44
Claudia Ziemba <claudia.ziemba@gmail.com>
2020-05-06 15:19:14 -0300

    Updates

---
+++ precache-manifest.5bd4021d67862bd43e9fa5a895d29d94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "055f2f751162e88511c3",
+    "url": "/robofriends/static/css/main.945affb2.chunk.css"
+  },
+  {
+    "revision": "055f2f751162e88511c3",
+    "url": "/robofriends/static/js/main.49f24aee.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "17349e503e0e5e6c5aa7",
+    "url": "/robofriends/static/css/2.389b6aef.chunk.css"
+  },
+  {
+    "revision": "17349e503e0e5e6c5aa7",
+    "url": "/robofriends/static/js/2.b98a55d3.chunk.js"
+  },
+  {
+    "revision": "1bd5ae91e160f33099f29fcdcaaf5ffe",
+    "url": "/robofriends/index.html"
+  }
+];
